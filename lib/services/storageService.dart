29450f1bb70badf4ff32d3c12f1da4ad481a0779
mehmetsonmezz <se.mehmetsonmez@gmail.com>
2022-07-03 05:09:59 +0300

    merge error fixed

--- lib/services/storageService.dart
+++ lib/services/storageService.dart
@@ -15,5 +15,30 @@ class StorageService {
     return uploadedImageURL;
   }
 
+  Future<List<Map<String, dynamic>>> getVenueGallery(String venueID) async {
+    List<Map<String, dynamic>> images = [];
+
+    final result = await _storageRef.storage.ref('venuePhotos/$venueID').list();
+    final files = result.items;
+
+    await Future.forEach<Reference>(files, (file) async {
+      final String fileUrl = await file.getDownloadURL();
+
+      images.add({
+        "url": fileUrl,
+        "path": file.fullPath,
+      });
+    });
+
+    return images;
+  }
+
+  Future<String> uploadVenueImage(File image) async {
+    imageID = const Uuid().v4();
+    final uploadTask =
+        _storageRef.child('venueProfileImages/$imageID').putFile(image);
+    final taskSnapshot = await uploadTask;
+    String uploadedImageURL = await taskSnapshot.ref.getDownloadURL();
+    return uploadedImageURL;
   }
 }
